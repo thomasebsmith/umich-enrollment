f24b14d8ea5bddc4372faeb458770c2e00ced2fe
Thomas Smith <thsm@umich.edu>
2020-04-04 15:38:34 -0400

    Create script and files to scrape F20 EECS courses

---
+++ captureData.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+term=""
+term_id=""
+filename="$(date -u '+%Y-%m-%dT%H:%M:%SZ').json"
+
+while IFS= read -r class; do
+  if [ "$term" = "" ]; then
+    term="$class"
+    term_id="$(echo "$term" | cut -d'_' -f3)"
+  else
+    sed "s/\\\$\\\$TERMID\\\$\\\$/$term_id/g" template.json > \
+      captureData_temp.json
+    sed -i '' "s/\\\$\\\$TERMARRAY\\\$\\\$/$term/g" captureData_temp.json
+    sed -i '' "s/\\\$\\\$CLASS\\\$\\\$/$class/g" captureData_temp.json
+    mkdir -p "./data/$term/$class/"
+    python3 ./scraper/__main__.py captureData_temp.json > \
+      "./data/$term/$class/$filename"
+  fi
+done < term
+rm -f captureData_temp.json
