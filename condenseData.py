1d5695f32cb49bb49a35287486e715943cc4f112
Thomas Smith <thsm@umich.edu>
2020-04-15 09:31:01 -0400

    Move site/ to docs/ to allow GitHub pages hosting

--- condenseData.py
+++ condenseData.py
@@ -31,7 +31,7 @@ for class_dir in class_dirs:
   if class_dir.is_dir():
     output_data[class_dir.name] = gather_data(class_dir.path)
 
-output_file = os.path.join("./site/data/", term + ".json")
+output_file = os.path.join("./docs/data/", term + ".json")
 output_file = open(output_file, "w")
 json.dump(output_data, output_file)
 
